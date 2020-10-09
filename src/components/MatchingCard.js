dfdfabffd8fa6b644d11f4d3e545ade7dda7b279
shohamc1 <shhmchk@gmail.com>
2020-10-10 00:31:36 +0800

    sample firebase call

--- src/components/MatchingCard.js
+++ src/components/MatchingCard.js
@@ -1,49 +1,48 @@
-import React from "react"
+import React, { useEffect } from "react"
 
+export default function MatchingCard({ header, items }) {
+  const Header = {
+    color: "black",
+    backgroundColor: "#BBD95C",
+    fontWeight: "bold",
+    width: "100%",
+    height: "50px",
+    flexStart: "start",
+    alignText: "bottom",
+  }
 
-export default function MatchingCard({header , items}){
-    const Header ={
-        color: "black",
-        backgroundColor: "#BBD95C",
-        fontWeight: "bold",
-        width: '100%',
-        height: '50px',
-        flexStart:'start',
-        alignText:"bottom"
-    }
+  const Container = {
+    padding: "15px",
+    display: "flex",
+    margin: "10px",
+    marginRight: "20px",
+    marignLeft: "20px",
+    flexDirection: "column",
+  }
 
-    const Container = {
-        padding: "15px",
-        display: "flex",
-        margin: '10px',
-        marginRight: '20px',
-        marignLeft: '20px',
-        flexDirection:"column"
-    }
+  const GreyCards = {
+    backgroundColor: "#F4F4F4",
+    marginTop: "15px",
+    borderRadius: "10px",
+    padding: "10px",
+  }
 
-    const GreyCards ={
-        backgroundColor: '#F4F4F4',
-        marginTop: '15px',
-        borderRadius: '10px',
-        padding: '10px'
-    }
+  console.log(items)
+  return (
+    <div style={Container}>
+      <div style={Header}>{header}</div>
 
-    console.log(items)
-    return(
-    <div style={Container} >
-        <div style={Header} >{header}</div>
-
-            {
-                items.map((value, index) => {
-                    return <div style={GreyCards} key={index}> 
-                        {value[0]} 
-                        <div> {value[1]} </div> 
-                        <div> {value[2]} </div>
-                        <div> {value[3]} </div>
-                        <div> {value[4]} </div>
-                    </div>
-                })
-            }
+      {items.map((value, index) => {
+        return (
+          <div style={GreyCards} key={index}>
+            {value[0]}
+            <div> {value[1]} </div>
+            <div> {value[2]} </div>
+            <div> {value[3]} </div>
+            <div> {value[4]} </div>
+          </div>
+        )
+      })}
     </div>
-    )
+  )
 }
--- src/pages/matching.js
+++ src/pages/matching.js
@@ -1,10 +1,11 @@
 import React from "react"
 import Layout from "../components/Layout"
+import MatchingCard from "../components/MatchingCard"
 
 const matching = () => {
   return (
     <Layout>
-      <div></div>
+      <div />
     </Layout>
   )
 }
