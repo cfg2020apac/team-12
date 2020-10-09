f73299537b119c7ec934fda0885953fbb87ea8c4
lrchema <hema@Hemas-MacBook-Pro-3.local>
2020-10-09 18:53:51 +0800

    Adds import statements

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import React from "react"
 import Home from "../components/landing/HomePage"
 import fbConfig from "../../config/fbConfig"
 import firebase from "firebase/app"
+import Layout from "../components/Layout"
+import HomePage from "../components/landing/HomePage"
 
 export default function Index() {
   return (
