af2835ff5616e4dd7e1cc24fbfdb151ad7efe5ba
shohamc1 <shhmchk@gmail.com>
2020-10-10 01:33:48 +0800

    update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -31,7 +31,7 @@ const Navbar = () => {
             alt=""
             loading="lazy"
           />
-          </div>
+        </div>
         <span class="font-semibold text-xl tracking-tight">
           New Hope Community Services
         </span>
@@ -83,7 +83,7 @@ const Navbar = () => {
         </div>
         <div>
           <a
-            href="#"
+            href="/login"
             class="inline-block text-sm px-4 py-2 leading-none border rounded text-gray-700 border-black hover:text-gray-600 hover:bg-gray-200 mt-4 lg:mt-0"
           >
             Login/Logout
