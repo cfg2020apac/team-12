16ba5a04c164ac7b57f5bda7ffd2830a64a02a4e
shohamc1 <shhmchk@gmail.com>
2020-10-10 03:58:37 +0800

    fix data async issue

--- src/pages/beneficiaryPage.js
+++ src/pages/beneficiaryPage.js
@@ -2,43 +2,57 @@ import React, { useState, useEffect } from "react"
 import Layout from "../components/Layout"
 import Profile from "../components/Profile"
 import BeneficiaryCard from "../components/BeneficiaryCard"
-import HDBCard from '../components/HDBCard'
+import HDBCard from "../components/HDBCard"
 import { Helmet } from "react-helmet"
 import firebase from "firebase"
 import "firebase/firestore"
 
-const BeneficiaryPage = ({beneficiaryData, name}) => {
+const BeneficiaryPage = ({ beneficiaryData, name }) => {
   const [cr, setCr] = useState()
   useEffect(() => {
-    if(beneficiaryData!== undefined && name !== undefined){
-    //console.log(beneficiaryData)
-    var ref = firebase
-      .firestore()
-      .collection("counselling_records")
-      .doc(name)
-      .get()
-      .then(function (snapshot) {
-        console.log("inside then")
-        setCr(snapshot.data())
-      })
+    if (beneficiaryData !== undefined && name !== undefined) {
+      //console.log(beneficiaryData)
+      var ref = firebase
+        .firestore()
+        .collection("counselling_records")
+        .doc(name)
+        .get()
+        .then(function (snapshot) {
+          console.log("inside then")
+          setCr(snapshot.data())
+        })
     }
   }, [beneficiaryData])
   console.log(cr)
-  if(beneficiaryData!== undefined && name !== undefined){
-  return ( <div>{beneficiaryData ? (
+  return (
     <Layout>
       <Helmet>
         <title>Beneficiary Page - New Hope Community Services</title>
       </Helmet>
-      <Profile name={name} age={beneficiaryData.age} marital={beneficiaryData.marital} occupation={beneficiaryData.occupation} />
-
-      <HDBCard         
-        header="HDB"
-        items={{"allocation":"Completed", "collection":"Completed", "doc_approval":"Completed", "eligibility":"Not Completed", "matching":"Not Completed"}} 
-        clientName={"Thomas"}
+      {beneficiaryData ? (
+        <Profile
+          name={name}
+          age={beneficiaryData.age}
+          marital={beneficiaryData.marital}
+          occupation={beneficiaryData.occupation}
         />
+      ) : (
+        <div></div>
+      )}
 
-        {/* Items should be sent as "Completed" "Pending" "Not Completed" */}
+      <HDBCard
+        header="HDB"
+        items={{
+          allocation: "Completed",
+          collection: "Completed",
+          doc_approval: "Completed",
+          eligibility: "Not Completed",
+          matching: "Not Completed",
+        }}
+        clientName={"Thomas"}
+      />
+
+      {/* Items should be sent as "Completed" "Pending" "Not Completed" */}
 
       <BeneficiaryCard
         header="Counselling"
@@ -47,10 +61,8 @@ const BeneficiaryPage = ({beneficiaryData, name}) => {
           ["8 Oct 2020", "First Session with Counsellor", "Text box"],
         ]}
       />
-
-
-    </Layout>)
-      : <div></div>}</div> ) 
-}}
+    </Layout>
+  )
+}
 
 export default BeneficiaryPage
