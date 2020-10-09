a29f2be6084255dd0023b69e8f67b30ded926622
Danial Ang <danialshah968@gmail.com>
2020-10-09 19:59:14 +0800

    BeneficiaryCard

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import React from "react"
-import Home from "../components/landing/HomePage"
-import fbConfig from "../../config/fbConfig"
-import firebase from "firebase/app"
+import HomePage from "../components/landing/HomePage"
+// import fbConfig from "../../config/fbConfig"
+// import firebase from "firebase/app"
+import Layout from '../components/Layout'
 
 export default function Index() {
   return (
